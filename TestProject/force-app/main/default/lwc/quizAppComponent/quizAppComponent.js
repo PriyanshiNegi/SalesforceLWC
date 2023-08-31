57b769bd25bfba140dcd6905fbb18042e6d01834
PriyanshiNegi <priyanshiii1103@gmail.com>
2023-08-31 15:37:48 +0530

    adding submit and reset functionality

--- TestProject/force-app/main/default/lwc/quizAppComponent/quizAppComponent.js
+++ TestProject/force-app/main/default/lwc/quizAppComponent/quizAppComponent.js
@@ -4,6 +4,7 @@ import { LightningElement } from 'lwc';
 export default class QuizAppComponent extends LightningElement {
     selected = {}//to store answers
     correctAnswers = 0 //to show result
+    isSubmitted = false //to show results
     quiz =[
         {
             id : "Question 1",
@@ -37,6 +38,7 @@ export default class QuizAppComponent extends LightningElement {
         }
     ] 
 
+    //if all are not selected to disable button
     get allNotSelected(){
         return !(Object.keys(this.selected).length === this.quiz.length)
     }
@@ -52,15 +54,32 @@ export default class QuizAppComponent extends LightningElement {
         //[Question 1]:a
     }
 
+    //getting correct answer count
     submitHandler(event){
+        this.isSubmitted =true
         //form always refershes the page so to prevent it
         event.preventDefault()
-        this.quiz.filter()
+        //filtering all questions, picking question as item
+        //this.selected[item.id] = this.selected[Question 1] = a
+        //means value stored in Question 1
+        let correct = this.quiz.filter(item=>this.selected[item.id] === item.correct)
+        //as filter will return length
+        this.correctAnswers = correct.length
+        console.log ("The Correct Answers are :",this.correctAnswers )
 
     }
 
+    //for resetting options
     resetHandler(){
+        this.selected = {}//reset back to empty
+        this.correctAnswers = 0;
+        //to remove prompt once reset
+        this.isSubmitted = false
+    }
 
+    //adding padding for prompt and colors according to answer  
+    get ifAllCorrect(){
+        return `slds-p-around_small slds-text-heading_large ${this.quiz.length == this.correctAnswers ? 'slds-text-color_success' : 'slds-text-color_error'}`
     }
 
     
