31a50883b818e3099df8d217e503fb30bfaccfe4
PriyanshiNegi <priyanshiii1103@gmail.com>
2023-09-01 22:27:13 +0530

    child to parent with event data

--- TestProject/force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ TestProject/force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -2,8 +2,12 @@ import { LightningElement } from 'lwc';
 
 export default class C2pModalComponent extends LightningElement {
     // inform Parent to close modal on button click
-    closeHandler(){//but how would Parent know that button was clicked , answer : through custom Event
-        const myEvent = new CustomEvent('close')
+    //but how would Parent know that button was clicked , answer : through custom Event
+    closeHandler(){
+        // to pass data with event use standard property "detail"
+        const myEvent = new CustomEvent('close',{
+            detail : "Modal Closed Successfully"
+        })
         this.dispatchEvent(myEvent)
     }
 }
--- TestProject/force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
+++ TestProject/force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
@@ -2,12 +2,15 @@ import { LightningElement } from 'lwc';
 
 export default class C2pParentComponent extends LightningElement {
     showModal=false
-
+    msg
+    
     clickHandler(event){
         this.showModal = true 
     }
 
     closeTheHandler(event){
+        // extracting the data
+        this.msg = event.detail
         this.showModal = false 
     }
 }
