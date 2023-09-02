595f873e525858fecc90302bcff2324a57a07c21
PriyanshiNegi <priyanshiii1103@gmail.com>
2023-09-02 16:38:44 +0530

    configuring compA to publish message to compB

---
+++ TestProject/force-app/main/default/lwc/lmsComponentA/lmsComponentA.js
@@ -0,0 +1,43 @@
+
+//importing wire service
+import { LightningElement, wire } from 'lwc';
+
+// Prerequistes for communication between components :
+
+//1- referencing a message channel in LWC
+import SampleChannel from "@salesforce/messageChannel/SampleMessageChannel__c"
+
+//2- importing LMS API according to use case , so here we want compA to publish something to compB
+import { MessageContext, publish} from 'lightning/messageService'
+
+export default class LmsComponentA extends LightningElement {
+
+    //property to take input from input tag
+    inputValue
+
+    //context to know which all components are listening to LMS and then use the component we want
+    @wire(MessageContext)
+    context //stored in property called context
+
+    //store the input
+    changeHandler(event){
+        this.inputValue = event.target.value
+    }
+
+    publishMessage(){
+
+        //message to publish
+        const message ={
+            //field in messageChannel check meta file
+            lmsData : {
+                value : this.inputValue
+            }
+        }
+
+        //to publish a message : publish(messageContext,messageChannel,message)
+        publish(this.context,SampleChannel,message)
+        //message = data to publish
+        //SampleChannel = name of imported channel
+        //this.context = denotes the current message context (object of MessageContext , check the property above)
+    }
+}
